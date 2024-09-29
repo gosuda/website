d296803dca6a1e3a4364056c73c7a5de57f16003
ice.lemons.mint <ice.lemons.mint@gmail.com>
2024-09-29 14:17:21 +0000

    Add golang.go

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,7 +19,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "golang.go"
     ];
 
     # Enable previews
