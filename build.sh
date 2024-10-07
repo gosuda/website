15736e3e7fa4346fc5961b09c9550c6aec50f473
lemon-mint <ice.lemons.mint@gmail.com>
2024-10-07 10:20:12 +0900

    feat(build): enhance build script to install required Go tools

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-bun install
-go generate ./...
-go run .
+bun install && \
+  go install golang.org/x/tools/cmd/stringer@latest && \
+  go install github.com/a-h/templ/cmd/templ@latest && \
+  go generate ./... && \
+  go run .
