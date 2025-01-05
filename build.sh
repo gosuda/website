6acfb4a093729242e4fb0725266cd415a9c175e0
snowmerak <snowmerak@outlook.com>
2025-01-05 13:59:00 +0900

    fix templ cli version to v0.3.819

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 bun install && \
   go install golang.org/x/tools/cmd/stringer@latest && \
-  go install github.com/a-h/templ/cmd/templ@latest && \
+  go install github.com/a-h/templ/cmd/templ@v0.3.819 && \
   go generate ./... && \
   go run .
